3c890675a365420d6d441b7ab66333af590ec892
Karida Sabr Nazari Masaeva <elmasaeva@protonmail.com>
2018-06-12 00:24:15 +0300

    expand-collable blocks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import i18next from 'i18next';
 import React, {Component} from 'react';
 import {hot} from 'react-hot-loader'
 import './App.css';
@@ -6,18 +7,43 @@ import {CitizenshipWhy, CitizenshipWho} from './Citizenship';
 import {TemporaryWhy, TemporaryWho} from './Temporary';
 import {ImmigrationWhy, ImmigrationWho} from './Immigration';
 
+class Block extends Component {
+    state = {expanded: false};
+    handleExpand = (event)=> {
+        event.preventDefault();
+        this.setState({expanded: !this.state.expanded});
+    };
+
+    render() {
+        const [title, ...rest] = this.props.children;
+        return (<section>
+            <a href="#expand" onClick={this.handleExpand} >{title}</a>
+            {this.state.expanded && (<section>{rest}</section>)}
+        </section>);
+    }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <CitizenshipWhy />
-        <CitizenshipWho />
+        <Block>
+            <h1>{i18next.t('Citizenship')}</h1>
+            <CitizenshipWhy />
+            <CitizenshipWho />
+        </Block>
 
-        <ImmigrationWhy />
-        <ImmigrationWho />
+        <Block>
+            <h1>{i18next.t('Immigration')}</h1>
+            <ImmigrationWhy />
+            <ImmigrationWho />
+        </Block>
 
-        <TemporaryWhy />
-        <TemporaryWho />
+        <Block>
+            <h1>{i18next.t('Temporary Stay')}</h1>
+            <TemporaryWhy />
+            <TemporaryWho />
+        </Block>
       </div>
     );
   }
